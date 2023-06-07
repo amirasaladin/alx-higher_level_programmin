121344e1b34373b8a8f0a08964eafa3e1488f68c
amira <amirasaladin@hotmail.com>
2023-06-07 05:32:54 -0700

    add files 2330

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if(number > 0):
-        ldigit = number % 10
-    print(ldigit, end='')
+    ldigit = number % 10
+    print("{}".format(ldigit), end='')
