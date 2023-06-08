94aaefb34003dba1c01bf8b06c1ec356f23ed3dc
amira <amirasaladin@hotmail.com>
2023-06-08 09:32:32 -0700

    add files 26423

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,7 +6,7 @@ def main():
     argv = sys.argv
     args_len = len(argv)
     total = 0
-    for i in range (1, args_len):
+    for i in range(1, args_len):
         total += int(argv[i])
     print("{}".format(total))
 
