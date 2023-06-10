77b389f3028dc5f381dfe5e11c780b8395ff342d
amira <amirasaladin@hotmail.com>
2023-06-10 06:03:16 -0700

    add files 27532

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = [c for c in my_string]
-    new_string = ""
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            my_string.remove(c)
-    for c in my_string:
-        new_string += c
-    return new_string
+    my_string = [c for c in my_string if c not in "cC"]
+    return "".join(my_string)
