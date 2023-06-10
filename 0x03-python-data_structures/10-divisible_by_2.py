1163b57c094509e3880db4b30a5564a364767624
amira <amirasaladin@hotmail.com>
2023-06-10 09:35:54 -0700

    add files 1622

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,8 @@
 def divisible_by_2(my_list=[]):
     li = []
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             li.append(True)
         else:
             li.append(False)
     return li
-        
-
