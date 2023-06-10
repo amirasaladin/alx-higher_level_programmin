e23546f6a81f3c6072cbf2c2f6807368d1b08ea5
amira <amirasaladin@hotmail.com>
2023-06-10 04:59:12 -0700

    add files 11263

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in len(my_string):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            del my_string[i]
-    return my_string
+    my_string = [c for c in my_string]
+    new_string = ""
+    for c in my_string:
+        if c == 'c' or c == 'C':
+            my_string.remove(c)
+    for c in my_string:
+        new_string += c
+    return new_string
