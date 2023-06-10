9ab3706bb0a4f2705e7f1146adc0b6c605989a90
amira <amirasaladin@hotmail.com>
2023-06-10 09:31:07 -0700

    add files 31395

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    li = []
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            li.append(True)
+        else:
+            li.append(False)
+    return li
+        
+
