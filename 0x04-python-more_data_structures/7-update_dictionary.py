ab33675e9f4dce48b464e62b0cf495d97df0d8eb
amira <amirasaladin@hotmail.com>
2023-06-14 12:46:01 -0700

    add files 29743

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,12 +2,5 @@
 def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
         a_dictionary[key] = value
-    else:
-        # check if the value for the key is a list
-        if isinstance(a_dictionary[key], list):
-            # if the value is a list, append the new value to it
-            a_dictionary[key].append(value)
-        else:
-            # if the value is not a list, create a list with the existing value and the new value
-            a_dictionary[key] = [a_dictionary[key], value]
     return a_dictionary
+    
