c26de6d2d6736e3de24337c1ed583c1e4156442a
amira <amirasaladin@hotmail.com>
2023-06-16 08:25:08 -0700

    add files 17064

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is []:
+    if my_list == []:
         return 0
     prod = 0
     total_w = 0
     for item in my_list:
         prod += item[0] * item[1]
         total_w += item[1]
-    return prod / total_w        
+    return prod / total_w
